421bc90551f588f68cb370db80b550739bd95aad
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2026-03-01 14:03:20 +0100

    Implement conditional phone number formatting
    Add conditional formatting for phone numbers with '00' prefix.

--- phone_formatter.py
+++ phone_formatter.py
@@ -6,6 +6,9 @@ phone_number = input()
 
 pattern = r'00'
 
-formatted_PN = re.sub(pattern,'+',phone_number,count=1)
-
-print(formatted_PN)
+if re.match(pattern,phone_number) :
+    formatted_PN = re.sub(pattern,'+',phone_number,count=1)
+    print(formatted_PN)
+else :
+    formatted_PN = phone_number
+    print(formatted_PN)
